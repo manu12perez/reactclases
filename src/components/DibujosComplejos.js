adc488e02f463ff02c5bbb4920acb5fcdc0698c6
manu12perez <manu12perez@gmail.com>
2024-10-14 11:03:27 +0200

    reactclases

---
+++ src/components/DibujosComplejos.js
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+class DibujosComplejos extends Component{
+
+    //VAMOS A DIBUJAR UNA SERIE DE NUEMERO EN FORMATO HTML
+    //UTILIZANDO UN ARRAY CON <li></li>
+    dibujarNumeros = () => {
+        //DECLARAMOS UN ARRAY PARA ALMACENAR EL DIBUJO
+        var lista = [];
+        //VAMOS A REALIZAR UN BUCLE PARA RELLENAR NUMEROS DINAMICOS
+        for (var i = 1; i <=7; i++) {
+            var numero = parseInt(Math.random() * 100) + 1;
+            //MEDIANTE EL METODO push DEL ARRAY IREMOS
+            //RELLENANDO EL CODIGO HTML
+            lista.push(<li key={i}>{numero}</li>);
+        }
+        return lista;
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Dibujos Complejos HTML</h1>
+                <ul>
+                    {this.dibujarNumeros()}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default DibujosComplejos;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DibujosComplejos from './components/DibujosComplejos';
+import Contador from './components/Contador';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DibujosComplejos />
   </React.StrictMode>
 );
 
